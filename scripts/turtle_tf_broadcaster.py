2ed65e8200fb6da9e8a5c8ca42b42d0417b9bd96
Alakeyska <Salamakhin67@gmail.com>
2021-10-30 01:30:13 +0300

    First commit

---
+++ scripts/turtle_tf_broadcaster.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+import rospy
+import tf
+from tf.transformations import quaternion_from_euler
+from turtlesim.msg import Pose
+
+
+
+def handle_turtle_pose(msg):
+	br = tf.TransformBroadcaster()
+	br.sendTransform((msg.x, msg.y, 0), quaternion_from_euler(0, 0, msg.theta), rospy.Time.now(), turtlename, "world")
+	
+if __name__ == '__main__':
+	rospy.init_node('tf_tutrle')
+	turtlename = rospy.get_param('~turtle_tf_name')
+
+	rospy.Subscriber('input_pose', Pose, handle_turtle_pose)
+	rospy.spin()
---
+++ scripts/turtle_tf_satellite.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+
+import rospy
+import tf
+import math
+from tf.transformations import quaternion_from_euler
+from turtlesim.msg import Pose
+
+
+
+def handle_turtle_pose(msg):
+	global counter
+	br = tf.TransformBroadcaster()
+	br.sendTransform((msg.x, msg.y, 0), quaternion_from_euler(0, 0, msg.theta), rospy.Time.now(), turtlename, "world")
+	satellite = tf.TransformBroadcaster()
+	satellite.sendTransform((math.sin(counter) * 1.5, math.cos(counter) * 1.5, 1), quaternion_from_euler(0, 0, counter), rospy.Time.now(), sat, "turtle1")
+	counter += 0.01
+
+	
+rospy.init_node('tf_tutrle')
+turtlename = rospy.get_param('~turtle_tf_name')
+sat = 'sat'
+counter = 0
+	
+
+rospy.Subscriber('input_pose', Pose, handle_turtle_pose)
+rospy.spin()
